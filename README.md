c72e0c0af7008458945e4d61c4cbe17a7b86de50
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-09-03 17:37:48 +0700

    feat(init git):

---
+++ README.md
@@ -0,0 +1,1 @@
+# dhupee-learning-jsx
---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "dhupee learning JSX Front End with Go Backend";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+
+        # Common packages list
+        package_list = with pkgs; [
+          # general utility
+          go-task
+
+          # Backend
+          go
+          air
+
+          # Frontend
+          nodejs_22
+        ];
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = package_list;
+
+            shellHook = ''
+              # make it impure by default, I don't care
+              export SHELL=$(which ${pkgs.zsh})
+              echo 'Development Shell Initialized'
+              exec zsh
+            '';
+          };
+        };
+
+        # packages.default = pkgs.stdenv.mkDerivation {
+        #   pname = "firmware";
+        #   version = "0.1.0";
+        #   src = ./.;
+        #   sandbox = false;
+        #   buildInputs = with pkgs; [
+        #     platformio-core
+        #     esptool
+        #   ];
+        #   buildPhase = "platformio run";
+        #   installPhase = "mkdir -p $out && cp -r .pio/build $out/";
+        # };
+      }
+    );
+}
