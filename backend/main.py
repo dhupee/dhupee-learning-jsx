e89c3879f674c937397feee59e13886599c958e5
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-10-07 22:55:58 +0700

    feat(add post processor and logging):

--- backend/main.py
+++ backend/main.py
@@ -17,8 +17,18 @@ class YoutubeData(BaseModel):
 # Init FastAPI
 app = FastAPI()
 
+# Configure basic logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+# Create a logger instance
+logger = logging.getLogger(__name__)
+
 # Create output directory for Youtube Downloader
 OUTPUT_DIR = randdir.create_output_dir()
+logger.warning("Output Directory: " + OUTPUT_DIR)
 
 
 # Endpoints
@@ -34,11 +44,13 @@ async def get_music(YTData: YoutubeData):
     url = YTData.URL
 
     if url == "":
+        logger.info("User not adding URL")
         return HTTPException(status_code=400, detail="No URL provided")
 
     # Download from youtube
     result = download_from_youtube(url)
 
+    # Return result
     if result:
         return {"message": "Music Downloaded"}
     else:
@@ -46,14 +58,33 @@ async def get_music(YTData: YoutubeData):
 
 
 def download_from_youtube(url: str) -> tuple[bool, str]:
+    """
+    Download MP3 file from Youtube URL
 
+    Args:
+        url: string
+
+    Returns:
+        success status: bool
+        result: string
+
+    """
+
+    # Youtube Downloader Parameters
     ydl_opts = {
-        "format": "bestaudio/best",
-        "extractaudio": True,
-        "audioformat": "mp3",
+        "format": "best",
         # "outtmpl": "%(title)s.%(ext)s",
         "outtmpl": os.path.join(OUTPUT_DIR, "%(title)s.%(ext)s"),
         "noplaylist": True,
+        "cookiesfrombrowser": ("firefox",),
+        # post process downloaded video, FFmpeg magic
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",  # Convert to MP3
+                "preferredquality": "192",  # Set desired MP3 quality (e.g., 192kbps)
+            }
+        ],
         # Metadata and thumbnail embedding
         "writethumbnail": True,  # Download the thumbnail
         "embedthumbnail": True,  # Embed it in the audio file
@@ -64,6 +95,7 @@ def download_from_youtube(url: str) -> tuple[bool, str]:
     if url == "":
         return False, "No URL provided"
 
+    # Attempt to download
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
         return True, "Success"
